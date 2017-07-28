af5928b666e82eada99362c83cae6c5ea92ae6e1
Yarkeev Denis <d.yarkeev@corp.mail.ru>
2017-07-28 16:04:58 +0300

    typescript

---
+++ src/onlineconf.ts
@@ -0,0 +1,195 @@
+import _ = require('lodash');
+import fs = require('fs');
+import path = require('path');
+import {EventEmitter} from 'events';
+
+export class OnlineConf extends EventEmitter {
+
+    protected config: any;
+    protected filename: string;
+
+    constructor(filename: string, options?: any) {
+        super();
+
+        options = _.assign({
+            rootDir: '/usr/local/etc/onlineconf',
+            isEnableWatch: true
+        }, options || {});
+
+        if (!options.isTest) {
+            this.filename = path.resolve(options.rootDir, filename + '.conf');
+
+            if (options.isEnableWatch) {
+                this._initWatcher();
+            }
+
+            if (options && options.callback) {
+                this.once('reload', options.callback);
+                this.reload();
+            } else {
+                this.reloadSync();
+            }
+        } else {
+            this.config = {};
+        }
+    }
+
+    getConfig(): any {
+        return this.config;
+    }
+
+    setConfig(config: any) {
+        this.config = config;
+    }
+
+    reload() {
+        let stream: any = fs.createReadStream(this.filename, {encoding: 'utf8'}),
+            config: any = {},
+            tail: string = '';
+
+        stream.on('data', (chunk: string) => tail = this._parse(tail + chunk, config));
+        stream.on('end', () => this._finalize(tail, config));
+    }
+
+    reloadSync() {
+        let data: string = fs.readFileSync(this.filename, {encoding: 'utf8'}) || '',
+            config: any = {},
+            tail: string = this._parse(data.trim(), config);
+
+        this._finalize(tail, config);
+    }
+
+    get(path: string): any {
+        return this.config[path];
+    }
+
+    set(path: string, value: any): OnlineConf {
+        this.config[path] = value;
+
+        return this;
+    }
+
+    isInited(): boolean {
+        return !!this.config;
+    }
+
+    protected _parse(chunk: string, config: any): string {
+        let lines: string[] = chunk.split(/\r?\n/);
+        let tail: string = lines.pop();
+
+        for (let i = 0; i < lines.length; i++) {
+            let line: string = lines[i],
+                m = line.match(/^\s*(\S+)\s+(.+)$/);
+
+            if (m) {
+                let key: string = m[1],
+                    value: string = m[2];
+
+                value = value.replace('\\n', '\n').replace('\\r', '\r');
+                let mJson: string[] = key.match(/^(.+):JSON$/);
+
+                if (mJson) {
+                    key = mJson[1];
+                    try {
+                        value = JSON.parse(value);
+                    } catch (e) {
+                        continue;
+                    }
+                }
+                config[key] = value;
+            }
+        }
+
+        return tail;
+    }
+
+    protected _finalize(tail: string, config: any) {
+        if (tail === '#EOF') {
+            this.config = config;
+            this.emit('reload', config);
+        } else {
+            this.emit('error', new Error('OnlineConf file is not finished with #EOF'));
+        }
+    }
+
+    protected _initWatcher() {
+        let basename: string = path.basename(this.filename);
+
+        fs.watch(path.dirname(this.filename), (event: Event, filename: string) => {
+            if (filename === basename) {
+                this.reload();
+            }
+        });
+    }
+
+}
+
+let onlineconf: OnlineConf;
+
+export function load(callback: Function, filename?: string, options?: any) {
+    if (!onlineconf) {
+        onlineconf = new OnlineConf(filename || 'TREE', _.assign({
+            callback: callback
+        }, options || {}));
+    } else if (onlineconf.isInited()) {
+        callback.call(onlineconf);
+    } else {
+        onlineconf.once('reload', callback);
+    }
+}
+
+export function loadSync(filename?: string, options?: any) {
+    if (!onlineconf || !onlineconf.isInited()) {
+        onlineconf = new OnlineConf(filename || 'TREE', options);
+    }
+
+    return onlineconf;
+}
+
+export function get(path: string, filename?: string, options?: any): any {
+    if (!onlineconf || !onlineconf.isInited()) {
+        loadSync(filename, options);
+    }
+
+    return onlineconf.get(path);
+}
+
+export function set(path: string, value: any, filename?: string, options?: any) {
+    if (!onlineconf || !onlineconf.isInited()) {
+        loadSync(filename, options);
+    }
+
+    return onlineconf.set(path, value);
+}
+
+export function on(eventName: string, handler: Function, filename?: string, options?: any) {
+    loadSync(filename, options);
+
+    onlineconf.on.apply(onlineconf, arguments);
+
+    return onlineconf;
+}
+
+export function emit() {
+    onlineconf.emit.apply(onlineconf, arguments);
+
+    return onlineconf;
+}
+
+export function initTest() {
+    if (!onlineconf || !onlineconf.isInited()) {
+        onlineconf = new OnlineConf(null, {
+            isTest: true
+        });
+    }
+
+    return onlineconf;
+}
+
+export function destroy() {
+    onlineconf = null;
+}
+
+export function getInstance() {
+    return onlineconf;
+}
---
+++ tests/OnlineconfSpec.js
@@ -0,0 +1,309 @@
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const OnlineConf = require('../dist/onlineconf');
+
+const TEST_DATA_DIR = './tests/data';
+
+describe('Onlineconf', () => {
+
+    describe('create instance', () => {
+
+        it('simple', () => {
+            let onlineconf = new OnlineConf.OnlineConf('test', {
+                rootDir: TEST_DATA_DIR
+            });
+
+            assert.equal(onlineconf instanceof OnlineConf.OnlineConf, true);
+        });
+
+        it('isTest', () => {
+            let onlineconf = new OnlineConf.OnlineConf('test', {
+                rootDir: TEST_DATA_DIR,
+                isTest: true
+            });
+
+            assert.deepEqual(onlineconf.getConfig(), {});
+        });
+
+        it('without eof', (done) => {
+            try {
+                new OnlineConf.OnlineConf('test-without-eof', {
+                    rootDir: TEST_DATA_DIR
+                });
+            } catch (err) {
+                assert.equal(err.message, 'OnlineConf file is not finished with #EOF');
+                done();
+            }
+        });
+
+        it('fail-json', () => {
+            let onlineconf = new OnlineConf.OnlineConf('test-fail-json', {
+                rootDir: TEST_DATA_DIR
+            });
+
+            assert.deepEqual(onlineconf.getConfig(), {'/foo/bar': 'foobar'});
+        });
+
+        it('watched', (done) => {
+            let filename = 'test-watched',
+                onlineconf;
+
+            if (!fs.existsSync('./.tmp')) {
+                fs.mkdirSync('./.tmp')
+            }
+
+            fs.writeFile(`./.tmp/${filename}.conf`, '/foo/bar foobar\n#EOF', (err) => {
+                onlineconf = new OnlineConf.OnlineConf(filename, {
+                    rootDir: './.tmp'
+                });
+
+                assert.equal(onlineconf.get('/bar/foo'), undefined);
+
+                onlineconf.on('reload', () => {
+                    assert.equal(onlineconf.get('/bar/foo'), 'barfoo');
+                    done();
+                });
+
+                fs.writeFile(`./.tmp/${filename}.conf`, '/foo/bar foobar\n/bar/foo barfoo\n#EOF', (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+            });
+        });
+
+    });
+
+    it('#getConfig()', (done) => {
+        let onlineconf = new OnlineConf.OnlineConf('test', {
+            rootDir: TEST_DATA_DIR
+        });
+
+        fs.readFile(path.resolve(TEST_DATA_DIR, 'test.conf'), (err, data) => {
+            let config = {};
+
+            data.toString()
+                .split('\n')
+                .filter((item) => item !== '#EOF')
+                .forEach((item) => {
+                    if (item.match(/^\s*(\S+)\s+(.+)$/)) {
+                        let parts = item.split(' '),
+                            key = parts[0],
+                            value = parts.slice(1).join(' ').replace('\\n', '\n').replace('\\r', '\r');
+
+                        if (key.match(/:JSON$/)) {
+                            key = key.replace(/:JSON$/, '');
+                            value = JSON.parse(value);
+                        }
+
+                        if (value) {
+                            config[key] = value;
+                        }
+                    }
+                });
+
+            assert.deepEqual(config, onlineconf.getConfig());
+
+            done();
+        });
+    });
+
+    it('#reload()', (done) => {
+        let filename = 'test-reload',
+            onlineconf;
+
+        if (!fs.existsSync('./.tmp')) {
+            fs.mkdirSync('./.tmp')
+        }
+
+        fs.writeFile(`./.tmp/${filename}.conf`, '/foo/bar foobar\n#EOF', (err) => {
+            onlineconf = new OnlineConf.OnlineConf(filename, {
+                rootDir: './.tmp',
+                isEnableWatch: false
+            });
+
+            fs.writeFile(`./.tmp/${filename}.conf`, '/foo/bar foobar\n/bar/foo barfoo\n#EOF', (err) => {
+                assert.equal(onlineconf.get('/bar/foo'), undefined);
+
+                onlineconf.on('reload', () => {
+                    assert.equal(onlineconf.get('/bar/foo'), 'barfoo');
+                    done();
+                });
+
+                onlineconf.reload();
+            });
+        });
+
+    });
+
+    it('#reloadSync()', (done) => {
+        let filename = 'test-mutable',
+            onlineconf;
+
+        if (!fs.existsSync('./.tmp')) {
+            fs.mkdirSync('./.tmp')
+        }
+
+        fs.writeFile(`./.tmp/${filename}.conf`, '/foo/bar foobar\n#EOF', (err) => {
+            onlineconf = new OnlineConf.OnlineConf(filename, {
+                rootDir: './.tmp',
+                isEnableWatch: false
+            });
+
+            fs.writeFile(`./.tmp/${filename}.conf`, '/foo/bar foobar\n/bar/foo barfoo\n#EOF', (err) => {
+                assert.equal(onlineconf.get('/bar/foo'), undefined);
+
+                onlineconf.reloadSync();
+
+                assert.equal(onlineconf.get('/bar/foo'), 'barfoo');
+
+                done();
+            });
+        });
+
+    });
+
+    it('#set()', () => {
+        let onlineconf = new OnlineConf.OnlineConf('test', {
+            rootDir: TEST_DATA_DIR
+        });
+
+        assert.equal(onlineconf.get('/foo/bar/foo/bar'), undefined);
+        onlineconf.set('/foo/bar/foo/bar', 'foobarfoobar');
+        assert.equal(onlineconf.get('/foo/bar/foo/bar'), 'foobarfoobar');
+    });
+
+    it('#isInited()', () => {
+        let onlineconf = new OnlineConf.OnlineConf('test', {
+            rootDir: TEST_DATA_DIR
+        });
+
+        onlineconf.config = null;
+
+        assert.equal(onlineconf.isInited(), false);
+        onlineconf.reloadSync();
+        assert.equal(onlineconf.isInited(), true);
+    });
+
+    describe('OnlineConf.load()', () => {
+
+        it('first create', (done) => {
+            OnlineConf.destroy();
+            OnlineConf.load(() => {
+                done();
+            }, 'test', {
+                rootDir: TEST_DATA_DIR
+            });
+        });
+
+        it('isInited === true', (done) => {
+            OnlineConf.load(() => {
+                OnlineConf.load(() => {
+                    done();
+                });
+            });
+        });
+
+        it('isInited === false', (done) => {
+            OnlineConf.load(() => {
+                OnlineConf.getInstance().setConfig(null);
+
+                OnlineConf.load(() => {
+                    done();
+                });
+
+                OnlineConf.getInstance().reload();
+            });
+        });
+
+    });
+
+    describe('OnlineConf.loadSync()', () => {
+
+        it('first create', () => {
+            OnlineConf.destroy();
+            OnlineConf.loadSync('test', {
+                rootDir: TEST_DATA_DIR
+            });
+        });
+
+    });
+
+    describe('OnlineConf.get()', () => {
+
+        it('first create', () => {
+            OnlineConf.destroy();
+            OnlineConf.get('/foo/bar', 'test', {
+                rootDir: TEST_DATA_DIR
+            });
+        });
+
+        it('simple', (done) => {
+            OnlineConf.destroy();
+            OnlineConf.load(() => {
+                assert.equal(OnlineConf.get('/foo/bar'), 'foobar');
+                done();
+            }, 'test', {
+                rootDir: TEST_DATA_DIR
+            });
+        });
+
+    });
+
+    describe('OnlineConf.set()', () => {
+
+        it('first create', () => {
+            let randomStr = Math.random().toString(32).slice(2);
+
+            OnlineConf.destroy();
+            OnlineConf.set('/foo/bar/foo/bar', randomStr, 'test', {
+                rootDir: TEST_DATA_DIR
+            });
+
+            assert.equal(OnlineConf.get('/foo/bar/foo/bar'), randomStr);
+        });
+
+        it('simple', (done) => {
+            OnlineConf.destroy();
+            OnlineConf.load(() => {
+                let randomStr = Math.random().toString(32).slice(2);
+
+                OnlineConf.set('/foo/bar/foo/bar', randomStr);
+
+                assert.equal(OnlineConf.get('/foo/bar/foo/bar'), randomStr);
+                done();
+            }, 'test', {
+                rootDir: TEST_DATA_DIR
+            });
+        });
+
+    });
+
+    describe('OnlineConf.on() && OnlineConf.emit()', () => {
+
+        it('simple', (done) => {
+            OnlineConf.destroy();
+
+            OnlineConf.on('test-event', done, 'test', {
+                rootDir: TEST_DATA_DIR
+            });
+
+            OnlineConf.emit('test-event');
+        });
+
+    });
+
+    describe('OnlineConf.initTest()', () => {
+
+        it('first create', () => {
+            OnlineConf.destroy();
+
+            OnlineConf.initTest();
+
+            assert.deepEqual(OnlineConf.getInstance().getConfig(), {});
+        });
+
+    });
+
+});
